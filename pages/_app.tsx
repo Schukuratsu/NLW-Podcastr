3c03032dbc1722745b608292f8aedae54d612a1a
Lucas Ennes <lucas.ennes@upperds.com>
2021-04-20 21:44:58 -0300

    criação do header

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,13 @@
-import '../styles/globals.scss'
+import { Header } from "../components/Header";
+import "../styles/globals.scss";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <Header />
+      <Component {...pageProps} />
+    </div>
+  );
 }
 
-export default MyApp
+export default MyApp;
